95c5d55485d7b0b2b67e1bdc80112db0fe8466d0
Mohamed Aachour <aachourmohameddeveloper@gmail.com>
2022-10-27 11:12:08 +0100

    Add props duscrution and validation

--- src/components/TodosList.js
+++ src/components/TodosList.js
@@ -1,18 +1,38 @@
 /* eslint-disable react/prop-types, react/destructuring-assignment */
 import React from 'react';
+import Proptypes from 'prop-types';
 import TodoItem from './TodoItem';
 
-const TodosList = (props) => (
+const TodosList = ({
+  todos,
+  handleChangeProps,
+  deleteTodoProps,
+  setUpdate,
+}) => (
   <ul>
-    {props.todos.map((todo) => (
+    {todos.map((todo) => (
       <TodoItem
         key={todo.id}
         todo={todo}
-        handleChangeProps={props.handleChangeProps}
-        deleteTodoProps={props.deleteTodoProps}
-        setUpdate={props.setUpdate}
+        handleChangeProps={handleChangeProps}
+        deleteTodoProps={deleteTodoProps}
+        setUpdate={setUpdate}
       />
     ))}
   </ul>
 );
+
+TodosList.propTypes = {
+  todos: Proptypes.arrayOf(
+    Proptypes.shape({
+      id: Proptypes.number.isRequired,
+      title: Proptypes.string.isRequired,
+      completed: Proptypes.bool.isRequired,
+    }),
+  ).isRequired,
+  handleChangeProps: Proptypes.func.isRequired,
+  deleteTodoProps: Proptypes.func.isRequired,
+  setUpdate: Proptypes.func.isRequired,
+};
+
 export default TodosList;
